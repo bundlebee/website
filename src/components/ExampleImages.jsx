f71801bd174cb9d774f35e39db9d5a92b701d1af
Adam Modras <25adam@gmail.com>
2018-08-16 12:36:23 -0700

    fixing staging branch

--- src/components/ExampleImages.jsx
+++ src/components/ExampleImages.jsx
@@ -1,21 +1,16 @@
+// @ts-nocheck
 import React from 'react';
 
-import step1 from './example-images/bundle-bee-step1.png';
-import step2 from './example-images/bundle-bee-step2.png';
-import step3 from './example-images/bundle-bee-step3.png';
-import step4 from './example-images/bundle-bee-step4.png';
-import step5 from './example-images/bundle-bee-step5.png';
+import step1 from './example-images/BundleBeeDemo-v2.gif';
 
 
 
 const ExampleImages = () => {
   return (
-    <div id="example-images">
-      <img src={step1} alt="example images" />
-      <img src={step2} alt="example images" />
-      <img src={step3} alt="example images" />
-      <img src={step4} alt="example images" />
-      <img src={step5} alt="example images" />
+    <div>
+      <div id="example-images">
+        <img src={step1} alt="example images" />
+      </div>
     </div>
   )
 };
--- src/components/MainArticle.jsx
+++ src/components/MainArticle.jsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React from 'react';
 import '../styles/main.css';
 import * as webpack from './webpack.svg';
@@ -11,7 +12,7 @@ const MainArticle = () => {
         <h2>{"SIMPLIFY YOUR BUILD PROCESS"}</h2>
         <h3>{"Test your React project against Webpack, Parcel and Rollup"}</h3>
         <div className="cards">
-          <div className="left">{"Bundle Bee enables your team to create a brand new  config file and show you the speed and size for each.    Our application also works for existing projects if you just want to see a visual representation of how long your existing build takes. "}</div>
+          <div className="left">{"Current web applications can grow immensely in a very short period of time.  Having the proper bundle tool is immensely useful for not just the developers but for the final product that is pushed to production.  Bundle Bee provides the ability for React developers to test, not just their current bundle configuration, but also compare it to other potential solutions to see which option is the most performant in terms of speed and bundle size.  This not only saves the developers time in setting up their bundle tools, but more importantly enables them to ship the fastest code for their users."}</div>
           <div className="right">{`Utilize Bundle Bee to visually represent the build speeds and sizes.  Compare against the top 3 bundle tools on the market:`} < br /> {`Webpack, Parcel & Rollup`}
             <div className="bundle-logos">
               <img src={webpack} alt="Webpack"></img>
